99731230e78edd2841dd61b2bafd126036807564
saldm04 <s.dimartino25@studenti.unisa.it>
2024-03-07 15:22:34 +0100

    Nuovo branch

--- src/main/java/provaGit/MainClass.java
+++ src/main/java/provaGit/MainClass.java
@@ -8,5 +8,15 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!\nCiao Mondo!");
+		
+		/*
+		 * a
+		 * rrrr
+		 * e
+		 * 
+		 * d
+		 * 
+		 * 
+		 * */
 	}
 }
