a258416e4f8ecde0f26ae1deeea05efdf9a83be8
saldm04 <s.dimartino25@studenti.unisa.it>
2024-03-07 15:31:27 +0100

    Main modificato nel main branch per creare un conflitto

--- src/main/java/provaGit/MainClass.java
+++ src/main/java/provaGit/MainClass.java
@@ -1,22 +1,8 @@
 package provaGit;
 
-/*
- * A big comment
- * */
-
 public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!\nCiao Mondo!");
-		
-		/*
-		 * a
-		 * rrrr
-		 * e
-		 * 
-		 * d
-		 * 
-		 * 
-		 * */
+		System.out.println("Hello world!!!!!");
 	}
 }
